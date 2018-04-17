aedd59a11e955bd0d7455bf10f485d00cb374e7c
Unknown <christian.wiche@alumnos.usm.cl>
2018-04-17 19:57:21 -0300

    Update:
    - Nueva organización del proyecto
    - Inicio de modificaciones en el firmware

--- Arduino/libraries/Steppers/cmake-build-debug/CMakeFiles/Steppers.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for Steppers.
-# This may be replaced when dependencies are built.
---
+++ Firmware/Steppers_v_1_0/checkout/Arduino/libraries/Steppers/cmake-build-debug/CMakeFiles/Steppers.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Steppers.dir/library.cpp.obj"
+  "libSteppers.pdb"
+  "libSteppers.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Steppers.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
